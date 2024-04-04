bbfd8b81f5695f5b2269a1453ec347f87ffee08a
Stefan Rifel <s.rifel23i@gmail.com>
2024-04-04 19:26:33 +0200

    feat: solution for exercice 3.2

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -8,4 +8,23 @@ uniform vec3 fragColor;
 
 void main() {
     FragColor = vec4(fragColor, 1.0);
+
+    //first
+    //FragColor = vec4(mix(fragColor1, fragColor2, fragValue), 1.0);
+
+    //secound
+    //vec3 color = mix(fragColor1, fragColor2, fragValue);
+    //FragColor = vec4(step(0.5, color.r), step(0.5, color.g), 0.0, 1.0);
+
+    //third
+    //vec3 color = mix(fragColor1, fragColor2, fragValue);
+    //FragColor = vec4(smoothstep(0.4, 0.6, color.r), smoothstep(0.4, 0.6, color.g), 0.0, 1.0);
+
+    //fourth
+    //vec3 color = sin(4 * mix(fragColor1, fragColor2, fragValue) * 3.14);
+    //FragColor = vec4(color, 1.0);
+
+    //fifth
+    //vec3 color = sin(4 * mix(fragColor1, fragColor2, fragValue) * 3.14);
+    //FragColor = vec4(step(0, color.r), step(0, color.g), 0.0, 1.0);
 }
